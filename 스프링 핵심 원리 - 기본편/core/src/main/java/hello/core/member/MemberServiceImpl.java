a46279f12198d7f091af0e410c8a2014475cbda1
JaeGeun.Song <jae99c@gmail.com>
2022-02-22 18:20:44 +0900

    Revise: develop member domain

---
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+// 구현체가 하나 만 있을 때는 인터페이스 옆에 Impl이라고 관례상 사용.
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
