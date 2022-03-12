674d412ad0719d6013856c4881b66a51c4af4087
JaeGeun.Song <jae99c@gmail.com>
2022-03-12 15:38:39 +0900

    Revise: Implement @ComponentScan, @Autowired

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 // 구현체가 하나 만 있을 때는 인터페이스 옆에 Impl이라고 관례상 사용.
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired // ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
