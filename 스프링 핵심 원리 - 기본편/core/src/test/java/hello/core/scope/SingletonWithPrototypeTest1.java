a71f4bbae00c25fe77bf426ba8f32161fe56214b
JaeGeun.Song <jae99c@gmail.com>
2022-03-21 18:28:33 +0900

    Test prototypeBeanProvider.getObject() [about ObjectProvider]

--- 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ 스프링 핵심 원리 - 기본편/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -1,6 +1,8 @@
 package hello.core.scope;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectFactory;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -34,19 +36,23 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
     @Scope("singleton")
-    static class ClientBean2 {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+    static class ClientBean {
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입
 //        @Autowired
 //        ApplicationContext applicationContext;
         @Autowired
-        public ClientBean2(PrototypeBean prototypeBean){
-            this.prototypeBean = prototypeBean;
-        }
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider;
+//        private ObjectFactory<PrototypeBean> prototypeBeanProvider;
+//        @Autowired
+//        public ClientBean2(PrototypeBean prototypeBean){
+//            this.prototypeBean = prototypeBean;
+//        }
         public int logic(){
 //            PrototypeBean prototypeBean = applicationContext.getBean(PrototypeBean.class);
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             return prototypeBean.getCount();
 
