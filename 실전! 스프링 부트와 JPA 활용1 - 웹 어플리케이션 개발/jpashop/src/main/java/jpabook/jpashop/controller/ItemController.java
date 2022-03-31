8b317922c7d0509e0f4896f8a23f73f481f1eedd
JaeGeun.Song <jae99c@gmail.com>
2022-03-31 22:48:47 +0900

    Revise: Implement add goods(book) layout & @Get, @Post Mapping Method

---
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/controller/ItemController.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.controller;
+
+import jpabook.jpashop.domain.item.Book;
+import jpabook.jpashop.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class ItemController {
+
+    private final ItemService itemService;
+
+    @GetMapping("/items/new")
+    public String createForm(Model model){
+        model.addAttribute("form", new BookForm());
+        return "items/createItemForm";
+    }
+
+    @PostMapping("/items/new")
+    public String create(BookForm form){
+        Book book = new Book(); // 사실 이렇게 set으로 넣기보다 createBook 메소드를 생성해서 넣는게 더 좋은 방법이다.
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+
+        itemService.saveItem(book);
+        return "redirect:/";
+    }
+}
