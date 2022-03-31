6bdac108de0c3438f233813c30c41e77fc6cd38c
JaeGeun.Song <jae99c@gmail.com>
2022-03-31 18:50:33 +0900

    Revise: Develop Order business logic

--- 실전! 스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
+++ 실전! 스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발/jpashop/src/main/java/jpabook/jpashop/domain/Item.java
@@ -36,7 +36,7 @@ public abstract class Item {
     /**
      * stock 감소
      */
-    public void removeStrock(int quantity){
+    public void removeStock(int quantity){
         int restStock = this.stockQuantity - quantity;
         if(restStock < 0){
             throw new NotEnoughStockException("need more stock");
