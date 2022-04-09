6f26451b0db3a07589d83c5eb7422eb81297fa92
JaeGeun.Song <jae99c@gmail.com>
2022-04-09 21:01:32 +0900

    Mapping of field & column

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,56 +16,7 @@ public class JpaMain {
         tx.begin(); // database Transaction 시작
 
         try {
-            // 비영속
-//            Member member = new Member();
-//            member.setId(2L);
-//            member.setName("helloB");
 
-            // 영속
-//            System.out.println("=== BEFORE ===");
-//            em.persist(member); // 생성
-//            em.detach(member); // 영속성 컨텍스트에서 제거
-//            System.out.println("=== AFTER ===");
-
-//            Member findMember1 = em.find(Member.class, 101L);
-//            Member findMember2 = em.find(Member.class, 101L);
-
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName());
-//            Member findMember = em.find(Member.class, 1L); //em에서 member 객체 가져오기
-//            System.out.println("findMember.id = " + findMember.getId());
-//            System.out.println("findMember.name = " + findMember.getName()); 조회
-
-//            em.remove(findMember); // 삭제 (DB에서 삭제)
-
-//            findMember.setName("HelloJPA"); // 수정
-
-//            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-//                    .setFirstResult(5)
-//                    .setMaxResults(8)
-//                    .getResultList();//JPQL로 전체 회원 검색
-//
-//            for (Member member: result){
-//                System.out.println("member.name = " + member.getName());
-//            }
-//            Member member1 = new Member(150L, "A");
-//            Member member2 = new Member(160L, "B");
-//
-//            em.persist(member1);
-//            em.persist(member2);
-
-            Member member = em.find(Member.class, 150L);
-
-//            member.setName("AAAAA");
-
-//            em.detach(member); //준 영속 상태로
-//            em.clear(); // 완전히 초기화
-
-//            Member member2 = em.find(Member.class, 150L); //select가 두번 나감
-
-            //em.close() 완전히 영속성 컨텍스트를 종료료
-
-           System.out.println("=================================");
 
             tx.commit();
         }catch(Exception e){
--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,38 +1,37 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
 
 @Entity
 public class Member {
-    public Member(){} //기본생성자가 필요.
-
-    public Member(Long id, String name){
-        this.id = id;
-        this.name = name;
-    }
     @Id
     private Long id;
-    private String name;
+
+    @Column(name = "name", nullable = false)
+    private String username;
+
     private int age;
 
-    public Long getId() {
-        return id;
-    }
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
 
-    public String getName() {
-        return name;
-    }
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private LocalDate testLocalDate;
+    private LocalDateTime testLocalDateTime; // 최신버전 쓰는경우 그냥 이거 쓰면됨 @Temporal 말고
 
+    @Lob // varchar를 넘어서는 큰 컨텐츠??
+    private String description;
 
+    public Member(){
+    } //기본생성자가 필요.
 }
