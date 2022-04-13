acb6fea7204365b8c9246b561195dc740e4f1963
JaeGeun.Song <jae99c@gmail.com>
2022-04-13 19:58:19 +0900

    Two-way Association and owners of Association

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -8,6 +8,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args){
@@ -30,13 +31,12 @@ public class JpaMain {
             member.setTeam(team);
             em.persist(member);
 
-            // 조회
             Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();            // 양방향 연관관계 member -> team -> member
 
-            //연관관계가 없음
-            Team findTeam = findMember.getTeam();// 가져오려면 두번해야됨
-            System.out.println("findTeam.name = " + findTeam.getName());
-
+            for (Member m : members){
+                System.out.println("m = " + m.getUsername());
+            }
             tx.commit();
         }catch(Exception e){
             tx.rollback();
