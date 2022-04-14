56bed793dfdc469b7d8381cb19d4bf6e76d37b55
JaeGeun.Song <jae99c@gmail.com>
2022-04-14 19:32:15 +0900

    Implement @ManyToOne, @OneToMany @OneToOne @ManyToMany for practice

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/jpashop/src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,10 +1,7 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
-import jpabook.jpashop.domain.Team;
-import org.graalvm.compiler.core.common.type.ArithmeticOpTable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -22,14 +19,7 @@ public class JpaMain {
         tx.begin(); // database Transaction 시작
 
         try {
-            Order order = new Order();
-            em.persist(order);
 
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
-            // 양방향 연관관계가 아니라면 이렇게 개발하는데 아무 문제가 없음음
            tx.commit();
         }catch(Exception e){
             tx.rollback();
