be929a732d0f4acc0c1b84aebdf1e017cef2cea5
JaeGeun.Song <jae99c@gmail.com>
2022-04-16 13:02:14 +0900

    Test @MappedSuperclass

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,15 +1,31 @@
 package hellojpa;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Entity
-public class Member {
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name", nullable = false)
     private String username;
+
+    @ManyToOne
+    @JoinColumn(name="TEAM_ID", insertable=false, updatable=false)
+    private Team team;
+
+//    @OneToOne
+//    @JoinColumn(name="LOCKER_ID")
+//    private Locker locker;
+//
+//    @OneToMany(mappedBy = "member")
+//    private List<MemberProduct> memberProducts = new ArrayList<>();
+
+
+
 
     public Member(){
     } //기본생성자가 필요.
---
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -0,0 +1,51 @@
+package hellojpa;
+
+import hellojpa.BaseEntity;
+import hellojpa.Member;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.ArrayList;
+
+
+@Entity
+public class Team extends BaseEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+    private String name;
+
+    @OneToMany
+    @JoinColumn(name="TEAM_ID")
+    private List<Member> members = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
+//    public void addMember(Member member) {
+//        member.setTeam(this);
+//        members.add(member);
+//    }
+}
